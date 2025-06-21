71eae66337d6828795267c89c6b8fdfc570ab6f5
S Sumesh <sumesh2003nov5@gmail.com>
2025-06-21 21:58:36 +0530

    Initial commit

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './authSlice';
+import chatReducer from './chatSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    chat: chatReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST'],
+      },
+    }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
